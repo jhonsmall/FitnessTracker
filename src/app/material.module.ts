f4ec2e160b6400dd95d7c191e5d0d607b6d4effb
fj_babel <francisco.guzman@babel.es>
2020-02-26 13:26:57 +0100

    Create new material module when import all the angular material packages that i need it

---
+++ src/app/material.module.ts
@@ -0,0 +1,10 @@
+//Module when we import all the angular material packages that i want to use in the app
+import { NgModule } from '@angular/core';
+
+
+
+@NgModule({
+  imports: [],
+  exports: []
+})
+export class MaterialModule { }
