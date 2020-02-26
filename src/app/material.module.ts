9191e8123830a695f5532846940a9cb2f65e0a19
fj_babel <francisco.guzman@babel.es>
2020-02-26 15:25:52 +0100

    Add Sign up form with angular material

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,18 @@
 //Module when we import all the angular material packages that i want to use in the app
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button'
-import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   imports: [MatButtonModule,
-            MatIconModule],
+            MatIconModule,
+            MatFormFieldModule,
+            MatInputModule],
   exports: [MatButtonModule,
-            MatIconModule]
+            MatIconModule,
+            MatFormFieldModule,
+            MatInputModule]
 })
 export class MaterialModule { }
