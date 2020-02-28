c51552807c425c5012c6556765e5d67bdbeeaacf
fj_babel <francisco.guzman@babel.es>
2020-02-28 13:42:14 +0100

    Add the logic when you press the buttons of the modal

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { StopTrainingComponent } from './stop-training.component';
 
@@ -9,18 +9,23 @@ import { StopTrainingComponent } from './stop-training.component';
 })
 export class CurrentTrainingComponent implements OnInit {
 
+  @Output() trainingExit = new EventEmitter();
   progress = 10;
   timer: any;
 
   constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
+   this.startTraining();
+  }
+
+  startTraining(){
     this.timer = setInterval(() => {
       this.progress = this.progress + 10;
-      if (this.progress >= 100){
+      if (this.progress >= 100) {
         clearInterval(this.timer);
-      }     
-    },200);
+      }
+    }, 200);
   }
 
   onStop(){
@@ -32,7 +37,11 @@ export class CurrentTrainingComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
+      if (result) {
+        this.trainingExit.emit();
+      } else {
+        this.startTraining();
+      }
     })
   }
 
